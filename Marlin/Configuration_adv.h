45c2b2f13efd1bbf6ab591ab8151d85e27a9604a
Rich C <richcarni@gmail.com>
2022-07-03 17:14:06 +1000

    Disable stepper power down on SD print complete
    Since the Cetus tends to fall (unless the friction bearing is sufficiently tightened) when power is cut to the Z-axis stepper, this firmware setting can be problematic. It is probably better to disable the default: disable all motors on completion of an SD-card print, and if the user wants to disable motors on completion of a print, this can easily be added as custom gcode in the slicer

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1287,7 +1287,7 @@
 
   #define SD_PROCEDURE_DEPTH 1              // Increase if you need more nested M32 calls
 
-  #define SD_FINISHED_STEPPERRELEASE true   // Disable steppers when SD Print is finished
+  #define SD_FINISHED_STEPPERRELEASE false   // Disable steppers when SD Print is finished
   #define SD_FINISHED_RELEASECOMMAND "M84"  // Use "M84XYE" to keep Z enabled so your bed stays in place
 
   // Reverse SD sort to show "more recent" files first, according to the card's FAT.
